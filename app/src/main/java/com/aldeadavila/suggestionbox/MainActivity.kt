7cfe06b829086f4a17706f1ad530883277f0e224
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2023-10-28 20:54:42 +0200

    splash screen

--- app/src/main/java/com/aldeadavila/suggestionbox/MainActivity.kt
+++ app/src/main/java/com/aldeadavila/suggestionbox/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.aldeadavila.suggestionbox.navigation.graph.auth.AuthNavGraph
+import com.aldeadavila.suggestionbox.navigation.graph.AuthNavGraph
 import com.aldeadavila.suggestionbox.ui.theme.AldeadavilaSuggestionBoxTheme
 import dagger.hilt.android.AndroidEntryPoint
 
--- app/src/main/java/com/aldeadavila/suggestionbox/navigation/screen/auth/AuthScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.aldeadavila.suggestionbox.navigation.screen.auth
-
-sealed class AuthScreen(val route: String) {
-    object  Login: AuthScreen("login")
-    object Register: AuthScreen("register")
-    object ForgotPasswordScreen: AuthScreen("forgot_password")
-    object ProfileScreen: AuthScreen("profile")
-}
