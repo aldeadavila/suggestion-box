01ec81080676a0353040efbf16b2a21d0175f0a1
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2024-04-07 21:10:45 +0200

    add logo and change name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.aldeadavila.suggestionbox"
         minSdk = 23
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.9"
+        versionCode = 10
+        versionName = "1.10"
   
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
