91d19a575953fdb1c19727d71f3b6a5a1d42e198
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2025-06-30 10:12:39 -0300

    up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.aldeadavila.suggestionbox"
         minSdk = 26
         targetSdk = 34
-        versionCode = 28
-        versionName = "1.28"
+        versionCode = 29
+        versionName = "1.29"
   
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
